ce377088f61c6f58646fc12f72c14504089a8993
ppwwyyxx <ppwwyyxxc@gmail.com>
2014-03-13 23:44:10 +0800

    more function

--- paper-downloader.py
+++ paper-downloader.py
@@ -1,18 +1,31 @@
 #!/usr/bin/env python2
 # -*- coding: UTF-8 -*-
 # File: paper-downloader.py
-# Date: Thu Mar 13 12:29:56 2014 +0800
+# Date: Thu Mar 13 14:48:46 2014 +0800
 # Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 
 import argparse
 import operator
+import string
+from urlparse import urlparse
 import os
 import sys
 import os.path
 import requests
 from bs4 import BeautifulSoup
 
+import handlers
+from text import levenshtein, title_beautify, parse_file_size
+
 GOOGLE_SCHOLAR_URL = "http://scholar.google.com/scholar?hl=en&q={0}&btnG=&as_sdt=1%2C5&as_sdtp="
+
+HANDLERS = [handlers.dl_acm_org]
+
+
+def judge(query, title):
+    q = ''.join([t for t in query if t in string.letters])
+    now = ''.join([t for t in title if t in string.letters]).lower()
+    return levenshtein(q, now) < 7
 
 def get_args():
     desc = 'Automatic Search and download paper'
@@ -27,26 +40,24 @@ def get_args():
     ret = parser.parse_args()
     return ret
 
-def parse_real_link(link):
+def parse_side_link(link):
     # parse arxiv
     return link
 
-def title_beautify(title):
-    return title
-
-def parse_file_size(size):
-    if size > 1000000:
-        return "{0:.2f}MB".format(float(size) / 1000000)
-    if size > 1000:
-        return "{0:.2f}KB".format(float(size) / 1000)
-    return "{0}B".format(size)
+def parse_title_link(link):
+    parsed = urlparse(link)
+    for h in HANDLERS:
+        test = h(parsed)
+        if test:
+            print "Get download link {0} from page {1}".format(test, link)
+            return test
+    return None
 
 def download(title, pdflink):
     title = title_beautify(title)
     print "Get paper: {0}".format(title)
 
     ofile = os.path.join(args.directory, title + '.pdf')
-    pdflink = parse_real_link(pdflink)
 
     print "Downloading to {0}...".format(ofile)
     print "PDF Link is {0}".format(pdflink)
@@ -72,32 +83,32 @@ def download(title, pdflink):
     print "Download finished."
 
 
-
 def parse_scholar(query):
     print "Searching on Google Scholar..."
     query = query.lower()
-    token_set = set(query.split())
-    r = requests.get(GOOGLE_SCHOLAR_URL.format(query))
-    print r.text.encode('utf-8')
-    text = open('/tmp/a.html').read()
+    #r = requests.get(GOOGLE_SCHOLAR_URL.format(query))
+    #text = r.text.encode('utf-8')
+    #with open("/tmp/a.html", 'w') as f:
+        #f.write(text)
+    text = open("/tmp/a.html", 'r').read()
     soup = BeautifulSoup(text)
     results = soup.findAll(attrs={'class': 'gs_r'})
-    candidates = []
     for rst in results:
-        real_title = rst.findAll('h3')[0].get_text()
-        now_token_set = set(real_title.lower().split())
-        simi = len(token_set & now_token_set)
-
+        h3 = rst.findAll('h3')[0]
+        real_title = h3.get_text()
+        if not judge(query, real_title):
+            continue
         findpdf = rst.findAll(attrs={'class': 'gs_ggs'})
         if not findpdf:
-            continue
-        pdflink = findpdf[0].find('a').get('href')
-        candidates.append((real_title, simi, pdflink))
-    candidates = sorted(candidates, key=operator.itemgetter(1), reverse=True)
-    if candidates[0][1] > 4:
-        t = candidates[0]
-        download(t[0], t[2])
-
+            url = h3.find('a').get('href')
+            url = parse_title_link(url)
+            if not url:
+                continue
+        else:
+            pdflink = findpdf[0].find('a').get('href')
+            pdflink = parse_side_link(pdflink)
+            download(real_title, pdflink)
+    return False
 
 def main():
     global args
---
+++ text.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python2
+# -*- coding: UTF-8 -*-
+# File: text.py
+# Date: Thu Mar 13 14:40:12 2014 +0800
+# Author: Yuxin Wu <ppwwyyxxc@gmail.com>
+
+stopwords = set(['of', 'from', 'as', 'to', 'a', 'an', 'in', 'into', 'on', 'onto',
+            'about', 'the', 'for', 'and', 'or', 'by'])
+
+def levenshtein(s1, s2):
+    if len(s1) < len(s2):
+        return levenshtein(s2, s1)
+
+    if len(s2) == 0:
+        return len(s1)
+
+    previous_row = xrange(len(s2) + 1)
+    for i, c1 in enumerate(s1):
+        current_row = [i + 1]
+        for j, c2 in enumerate(s2):
+            insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + 1       # than s2
+            substitutions = previous_row[j] + (c1 != c2)
+            current_row.append(min(insertions, deletions, substitutions))
+        previous_row = current_row
+    return previous_row[-1]
+
+def title_beautify(title):
+    tk = title.title().split()
+    for (idx, w) in enumerate(tk):
+        if w.lower() in stopwords and not title.startswith(w):
+            tk[idx] = w.lower()
+    return " ".join(tk)
+
+def parse_file_size(size):
+    if size > 1000000:
+        return "{0:.2f}MB".format(float(size) / 1000000)
+    if size > 1000:
+        return "{0:.2f}KB".format(float(size) / 1000)
+    return "{0}B".format(size)
