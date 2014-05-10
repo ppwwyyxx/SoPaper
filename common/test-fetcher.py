c5ed48479b00f578c24c54a224c84a4575f15c76
ppwwyyxx <ppwwyyxxc@gmail.com>
2014-05-11 00:34:58 +0800

    can downlaod

--- common/test-fetcher.py
+++ common/test-fetcher.py
@@ -1,31 +1,26 @@
 #!/usr/bin/env python2
 # -*- coding: UTF-8 -*-
 # File: test-fetcher.py
-# Date: Sun May 11 00:16:33 2014 +0800
+# Date: Sun May 11 00:26:39 2014 +0800
 # Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 
 from fetcher import register_parser, SearchResult
 from job import JobContext
 
 if __name__ == '__main__':
+    ctx = JobContext("Test Filename")
+
     parser = register_parser.parser_dict['arxiv.org']
     sr = SearchResult(None, "http://arxiv.org/abs/1312.6680")
-    ctx = JobContext("Test Filename")
-    params = parser.run(ctx, sr)
-    print params
 
     #parser = register_parser.parser_dict['dl.acm.org']
     #url = "http://dl.acm.org/citation.cfm?id=2366157"
     #url2 = "http://dl.acm.org/citation.cfm?id=322274"
     #sr = SearchResult(None, url)
-    #ctx = JobContext("Test Filename")
-    #params = parser.run(ctx, sr)
-    #print params
-    #with open('/tmp/a', 'w') as f:
-        #print >> f, ctx.data
 
-    #func = register_parser.parser_dict['ieeexplore.ieee.org']
+    #parser = register_parser.parser_dict['ieeexplore.ieee.org']
     #sr = SearchResult(None, "http://ieeexplore.ieee.org/stamp/stamp.jsp?arnumber=00726791")
-    #print func(sr)
 
+    params = parser.run(ctx, sr)
+    print params
 
--- webapi/api/download.py
+++ webapi/api/download.py
@@ -1,16 +1,25 @@
 #!/usr/bin/env python2
 # -*- coding: UTF-8 -*-
 # File: download.py
-# Date: Sat May 10 20:32:13 2014 +0800
+# Date: Sun May 11 00:33:28 2014 +0800
 # Author: Yuxin Wu <ppwwyyxxc@gmail.com>
 
 from . import app, make_response, request
+from ukdbconn import get_mongo
 
 # api: /download?pid=1
 @app.route('/download')
 def download():
-    pid = request.values.get('pid')
-    data = open("/home/wyx/.vimrc").read()
+    pid = long(request.values.get('pid'))
+    db = get_mongo('paper')
+
+    doc = list(db.find({'_id': pid}).limit(1))
+    if not doc:
+        return make_response(''), 404
+    doc = doc[0]
+    data = doc['pdf']
+    with open('/tmp/a.pdf', 'w') as f:
+        f.write(data)
     resp = make_response(data)
-    resp.headers['Content-Disposition'] = "attachment; filename=test space.vimrc"
+    resp.headers['Content-Disposition'] = "attachment; filename={0}.pdf".format(doc['title'])
     return resp
