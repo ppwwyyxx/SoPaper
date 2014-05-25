ff6d9030cce72203b29d973de1e0a93247063401
LeeTZ <litiezheng513@gmail.com>
2014-05-25 10:59:15 +0800

    finish meta-info display

--- webapi/static/javascript/search.js
+++ webapi/static/javascript/search.js
@@ -31,7 +31,9 @@ $(document).ready(function() {
 */
 
 function SearchCtrl($scope, $http) {
-    $scope.paper_title = "abc";
+    $scope.paper_title = "So easy";
+    $scope.hasbibtex = false;
+    $scope.hasreferences = false;
     $scope.Search = function() {
         $.ajax({
             type: 'GET',
@@ -48,21 +50,37 @@ function SearchCtrl($scope, $http) {
             $scope.paper_title = data.results[0].title;
             $scope.paper_abstract = data.results[0].abstract;
             $scope.paper_author = data.results[0].author;
+            if (typeof $scope.paper_author != 'undefined')
+                $scope.hasauthor = true;
             $scope.paper_bibtex = data.results[0].bibtex;
+            if (typeof $scope.paper_bibtex != 'undefined')
+                $scope.hasbibtex = true;
             $scope.paper_citedby = data.results[0].citedby;
+            if (typeof $scope.paper_citedby != 'undefined')
+                $scope.hascitedby = true;
             if ($scope.paper_citedby) {
                 $scope.paper_citednum = $scope.paper_citedby.length;
             }
             $scope.paper_references = data.results[0].references;
-            $scope.paper_abstract = data.results[0].abstract;
+            if (typeof $scope.paper_references != 'undefined')
+                $scope.hasreferences = true;
             $scope.paper_download_count = data.results[0].download_cnt;
 
-            //$scope.paper_title = data.results[0].title;
             $scope.$digest();
             $(".ui.labeled.icon.sidebar")
                 .sidebar("show");
             $(".ui.extremly.wide.sidebar")
                 .sidebar("hide");
+
+            $('.ui.bib.modal').modal('setting', 'closable', false)
+                .modal('attach events', '.bib.button', 'show');
+
+            $('.ui.ref.modal').modal('setting', 'closable', false)
+                .modal('attach events', '.ref.button', 'show');
+
+            $('.ui.com.modal').modal('setting', 'closable', false)
+                .modal('attach events', '.com.button', 'show');
+
         }).error(function(data, status) {
             console.log('status' + status);
             console.log(data);
