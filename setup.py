46b574a11aa64abacb30301734bdfed8f9992653
Yuxin Wu <ppwwyyxxc@gmail.com>
2016-01-03 20:18:46 -0500

    add setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'sopaper',
+  packages = ['sopaper'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Automatically search and download paper',
+  author = 'Yuxin Wu',
+  author_email = 'ppwwyyxxc@gmail.com',
+  url = 'https://github.com/ppwwyyxx/sopaper', # use the URL to the github repo
+  download_url = 'https://github.com/ppwwyyxx/sopaper/tarball', # I'll explain this in a second
+  keywords = ['paper', 'academic', 'crawler'], # arbitrary keywords
+  classifiers = [],
+)
